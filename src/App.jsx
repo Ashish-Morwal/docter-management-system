970a2d8738e71e9d48ed57899de4c2f37d3fb42f
Ashish Morwal <126047374+Ashish-Morwal@users.noreply.github.com>
2025-07-28 21:36:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
 
 const App = () => {
   return (
